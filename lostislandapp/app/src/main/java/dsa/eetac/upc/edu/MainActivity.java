cbebafa2be39934c38d60066d3d3d8d463e5486c
SergiVera <sergitus1998@gmail.com>
2018-12-24 13:46:49 +0100

    export unity as androidapp

---
+++ lostislandapp/app/src/main/java/dsa/eetac/upc/edu/MainActivity.java
@@ -0,0 +1,24 @@
+package dsa.eetac.upc.edu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button unityButton = findViewById(R.id.launchgamebtn);
+        unityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ConfigurationActivity.class));
+            }
+        });
+    }
+}
