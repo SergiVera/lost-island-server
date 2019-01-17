69d34fa06d9357987f893698baaabc2190f061a8
SergiVera <sergitus1998@gmail.com>
2019-01-17 22:54:53 +0100

    login and signup web finally finished

--- lostIsland/public/js/login.js
+++ lostIsland/public/js/login.js
@@ -1,3 +1,7 @@
+//Global uri
+var BASE_URI = "http://147.83.7.155:8080/dsaApp/";
+
+//Calls when the document is ready
 $(document).ready(function(){
     //Code if the user clicks log in button
     $("#loginbutton").click(function(){
@@ -15,12 +19,16 @@ $(document).ready(function(){
       $.ajax({  
         headers: { 'content-type': 'application/json',"x-kii-appid": "XXXXX","x-kii-appkey":"XXXXX" },
         type: 'POST',
-        url:  "http://147.83.7.205:8080/dsaApp/auth/login",
+        url:  BASE_URI.concat("auth/login"),
         data: JSON.stringify(user),
         dataType: 'json',
         success: function(data) {
             console.log("Log in succesfully");
             console.log(data);
+            console.log(url);
+            var url1 = "http://147.83.7.155:8080/html/index2.html?username=";
+            var url = url1.concat(username);
+            window.open(url, "_self");
         },
         error: function(error){
           if(error.status==402){
--- lostIsland/public/js/signup.js
+++ lostIsland/public/js/signup.js
@@ -1,3 +1,8 @@
+//Global uri
+var BASE_URI = "http://147.83.7.155:8080/dsaApp/";
+ 
+//Calls when the document is ready
+$(document).ready(function(){
  //Code if the user clicks sign up button
  $("#signupbutton").click(function(){
     var username = $(".username").val();
@@ -14,12 +19,14 @@
     $.ajax({  
       headers: { 'content-type': 'application/json',"x-kii-appid": "XXXXX","x-kii-appkey":"XXXXX" },
       type: 'POST',
-      url:  "http://147.83.7.205:8080/dsaApp/auth/sign-up",
+      url:  BASE_URI.concat("auth/sign-up"),
       data: JSON.stringify(user),
       dataType: 'json',
       success: function(data) {
-          console.log("Log in succesfully");
+          console.log("Sign up successfully");
           console.log(data);
+          alert("User successfully created");
+          window.open("http://147.83.7.155:8080","_self");
       },
       error: function(error){
         if(error.status==402){
@@ -27,4 +34,5 @@
         }
       }
   }); 
+});
 });
